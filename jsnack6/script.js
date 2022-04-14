4b1bcef03a5a9f1a6225fd0ae64fd9748a0cd435
ketjj <jorjolianiket@gmail.com>
2022-04-14 17:32:04 +0200

    jsnack 6 + jsnack 9

--- jsnack6/script.js
+++ jsnack6/script.js
@@ -2,3 +2,14 @@
  JSnack 6
  Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 */
+
+const n = parseInt(prompt('Inserisci un numero')); 
+const limit = 9;
+
+let result;
+for(i = 1; i <= 9; i++){
+  result = Math.pow(n, 3);
+  console.log (result);
+}
+
+document.getElementById('result').innerHTML = result;
--- jsnack9/script.js
+++ jsnack9/script.js
@@ -3,4 +3,23 @@
  Calcola la somma e la media dei primi 10 numeri.
 */
 
+let totNumbers = 10;
+totNumbers = Math.ceil(Math.random() * 10) +1;
+console.log(totNumbers);
 
+let totSm = 0;
+let media = 0;
+
+for(let i = 1; i <= totNumbers; i++){
+  console.log('Numeri: 1 a 10', i);
+  totSm += i
+  console.log('Numeri sommati', totSm);
+}
+
+media = totSm / totNumbers;
+console.log('Numeri divisi', media);
+
+document.getElementById('result').innerHTML = 
+`
+La somma dei primi 10 numeri random &egrave; ${totSm}, mentre la media &egrave; ${media}.
+`;
