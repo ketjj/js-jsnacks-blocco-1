2bd4b87ef25993704e770bef8c32b7e4c1b76d38
ketjj <jorjolianiket@gmail.com>
2022-04-16 16:01:06 +0200

    jsnack7

--- jsnack7/script.js
+++ jsnack7/script.js
@@ -5,3 +5,15 @@
  perciò per fermarsi prima di 1000 bisogna stampare la potenza prima di incrementarla
  perché altrimenti il controllo viene fatto dopo e si sfora
 */
+
+let listP = [];
+let numberP = 2;
+let startNumber = 2;
+
+while(startNumber < 1000) {
+  listP.push(startNumber);
+  startNumber = startNumber * numberP;
+  console.log(startNumber);
+}
+
+document.getElementById('result').innerHTML = "La potenza di 2 sono: " + listP;
