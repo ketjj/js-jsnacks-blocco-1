18e27b034a8627c312422dc5fad11a5c16d691da
ketjj <jorjolianiket@gmail.com>
2022-04-17 22:47:14 +0200

    update jnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,14 +3,28 @@
  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 */
 
-const firstNumber = parseInt(prompt('Inserisci il primo numero'));
-const secondNumber = parseInt(prompt('Inserisci il secondo numero'));
+let firstNumber = prompt('Inserisci il primo numero');
 
 console.log(firstNumber);
-console.log(secondNumber);
+// console.log(secondNumber);
+
+  while (isNaN(firstNumber)) {
+    alert('Non hai inserito un numero');
+    firstNumber = prompt('Inserisci il primo numero');
+  }
+  firstNumber = parseInt(firstNumber);
+  
+  let secondNumber = prompt('Inserisci il secondo numero');
+  
+  while (isNaN(secondNumber)) {
+  alert('Non hai inserito un numero');
+  secondNumber = prompt('Inserisci il secondo numero');
+}
+
+secondNumber = parseInt(secondNumber);
 
 if(firstNumber > secondNumber){
-  document.getElementById('result').innerHTML = firstNumber;
-} else {
-  document.getElementById('result').innerHTML = secondNumber;
+ document.getElementById('result').innerHTML = firstNumber;
+ } else {
+ document.getElementById('result').innerHTML = secondNumber;
 }
