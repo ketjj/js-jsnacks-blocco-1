9c770e5bc944fdfe7505344f7d969a1cd6037f0a
ketjj <jorjolianiket@gmail.com>
2022-04-18 00:00:34 +0200

    update jnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -11,13 +11,13 @@ const secondaParola = prompt('Inserisci una città');
 console.log(primaParola);
 console.log(secondaParola);
 
+
 if(primaParola.length > secondaParola.length) {
   // console.log(secondaParola);
   document.getElementById('result').innerHTML = secondaParola + " " +primaParola;
 
   
-} else if (secondaParola.length > primaParola.length) {
+} else if (secondaParola.length >= primaParola.length) {
   // console.log(primaParola);
   document.getElementById('result').innerHTML = primaParola + " " + secondaParola;
- 
-}
+} 
